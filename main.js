ce6804cb4102f489433fb1c9dd75bde1a652478c
enemrocky <craigcharltonlunga1000@gmail.com>
2022-11-29 22:23:40 -0800

    first commit

---
+++ main.js
@@ -0,0 +1,33 @@
+function myFunction(){
+    var x = document.getElementById("mobile__links");
+    if (x.style.display === "none"){
+        x.style.display = "block";
+    }else{
+        x.style.display = "none"
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
